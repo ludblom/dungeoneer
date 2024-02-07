8e61ded53eb3cd02452bfea2e91ad4bfa9392ce9
Ludvig Blomkvist <me@ludblom.net>
2024-02-07 20:57:11 +0100

    Changed class selection to proficency and class

--- dungeoneer-api/src/character.rs
+++ dungeoneer-api/src/character.rs
@@ -1,13 +1,15 @@
-use dungeoneer_types::{character::{Character, Gender, AbilityScore, Language}, class::Class, race::Race};
+use dungeoneer_types::{character::{Character, Gender, AbilityScore, Language}, class::Class, race::{Race, RaceSelection, Proficiency}};
 
 pub fn new_character(
     name: String,
     gender: Gender,
     languages: Vec<Language>,
     class: Class,
-    race: Race,
+    race_selection: RaceSelection,
+    proficiency: Proficiency,
     ability_score: AbilityScore
 ) -> Character {
+    let race: Race = Race::new(race_selection, proficiency);
     let character = Character::new(name, gender, languages, class, race, ability_score);
     character
 }
