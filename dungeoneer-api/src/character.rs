5a1fa6d8f10e7ce742a8176f25227e19a6aa5b64
Ludvig Blomkvist <me@ludblom.net>
2024-03-07 02:28:54 +0100

    Added Dwarf classes

--- dungeoneer-api/src/character.rs
+++ dungeoneer-api/src/character.rs
@@ -1,4 +1,6 @@
-use dungeoneer_types::{character::{Character, Gender, AbilityScore, Language}, class::Class, race::{Race, RaceSelection, Proficiency}};
+use std::collections::HashMap;
+
+use dungeoneer_types::{character::{Character, Gender, AbilityScore, Language}, class::Class, race::{Race, RaceSelection, Proficiency, AbilityScoreOptions, Resilience, Unique}};
 
 pub fn new_character(
     name: String,
@@ -9,7 +11,50 @@ pub fn new_character(
     proficiency: Proficiency,
     ability_score: AbilityScore
 ) -> Character {
-    let race: Race = Race::new(race_selection, proficiency);
+    let race: Race = {
+        let proficiency = proficiency;
+        match race_selection {
+            RaceSelection::HillDwarf => {
+                let race = Race {
+                    race_selection,
+                    ability_score_increase: HashMap::from([
+                        (AbilityScoreOptions::Constitution.to_string(), 2),
+                        (AbilityScoreOptions::Wisdom.to_string(), 1),
+                    ]),
+                    resilience: vec![
+                        Resilience::Poison,
+                    ],
+                    proficiency: vec![
+                        proficiency
+                    ],
+                    unique: vec![
+                        Unique::DwarvenToughness,
+                    ]
+                };
+                race
+            },
+            RaceSelection::MountainDwarf => {
+                let race = Race {
+                    race_selection,
+                    ability_score_increase: HashMap::from([
+                        (AbilityScoreOptions::Constitution.to_string(), 2),
+                        (AbilityScoreOptions::Strength.to_string(), 2),
+                    ]),
+                    resilience: vec![
+                        Resilience::Poison,
+                    ],
+                    proficiency: vec![
+                        proficiency,
+                        Proficiency::LightArmor,
+                        Proficiency::MediumArmor,
+                    ],
+                    unique: Vec::new(),
+                };
+                race
+            },
+            _ => todo!("Not implemented."),
+        }
+    };
     let character = Character::new(name, gender, languages, class, race, ability_score);
     character
 }
